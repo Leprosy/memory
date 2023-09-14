7fcbb4b1e1054821bb021a63109a03e619c7b2d6
Miguel Rojas <leprosy57@gmail.com>
2023-09-13 22:02:59 -0300

    Image fetching from endpoint. Back asset.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,6 @@
 import { StateContext } from "../state/StateContext";
 import { Tile } from "./Tile";
-import { useContext, useEffect } from "react";
+import { useContext, useState, useEffect } from "react";
 
 const getToggledItemList = (items, index) => {
   return items.map(item => {
@@ -38,12 +38,16 @@ const checkClicked = (items) => {
   return val;
 };
 
+
+
 export const Board = () => {
   const { items, setItems, score, setScore, errors, setErrors } = useContext(StateContext);
+  const [images, setImages] = useState([]);
   console.log("Board: items", items);
 
   const tiles = items.map((item) => {
     return <Tile
+      image={images[item.value]}
       key={item.index}
       index={item.index}
       value={item.value}
@@ -73,10 +77,28 @@ export const Board = () => {
     }
   }, [items]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("https://fed-team.modyo.cloud/api/content/spaces/animals/types/game/entries?per_page=20");
+      const images = await res.json();
+      const list = images.entries.map(item => item.fields.image.url);
+      setImages(list);
+      console.log("Images fetched", list);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">This is board</h1>
-      {tiles}
+      {images.length == 0
+        ? (<h1 className="text-3xl font-bold underline">Loading board...</h1>)
+        : <>
+          <h1 className="text-3xl font-bold underline">This is board</h1>
+          {tiles}
+        </>
+      }
+
     </div>
   );
 };
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,14 +1,21 @@
+import back from "../assets/back.jpg";
+
 export const Tile = (props) => {
-  const { value, visible, clicked, index, onPress } = props;
+  const { value, visible, clicked, index, image, onPress } = props;
 
   return (
     <div className="bg-sky-500" onClick={() => {
       console.log(`${value} clicked`);
       onPress(index);
-    }}>
+    }} style={{ float: "left" }}>
       <h1 className="text-3xl">Tile {index} = {value}</h1>
       <h3>visible: {visible ? "Yes" : "No"}</h3>
       <h3>clicked: {clicked ? "Yes" : "No"}</h3>
+      {visible ?
+        <img src={image} style={{ width: 100, height: 100 }} />
+        :
+        <img src={back} style={{ width: 100, height: 100 }} />
+      }
     </div>
   );
 };
