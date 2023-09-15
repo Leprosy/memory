3b1ae07a9262409e03202a1e74ec2dbbb0cb2cbb
Miguel Rojas <leprosy57@gmail.com>
2023-09-15 15:49:07 -0300

    Updated tests

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -4,5 +4,7 @@ import App from "../App";
 test("renders the screen", () => {
   render(<App />);
   expect(screen.getByText(/Memory Game/)).toBeInTheDocument();
+  expect(screen.getByText(/Ok/)).toBeInTheDocument();
+  expect(screen.getByText(/Restart/)).toBeInTheDocument();
   expect(screen.getByText("(C)2023")).toBeInTheDocument();
 });
