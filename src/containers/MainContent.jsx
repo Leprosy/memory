4d43b0a1b4f3948a36315830a41497e670e496d0
Miguel Rojas <leprosy57@gmail.com>
2023-09-15 15:02:16 -0300

    User needs to be logged to play

--- src/containers/MainContent.jsx
+++ src/containers/MainContent.jsx
@@ -1,7 +1,16 @@
 import { Board } from "../components/Board";
 import { Score } from "../components/Score";
+import { useContext } from "react";
+import { UserContext } from "../state/UserContext";
+
 
 export const MainContent = () => {
+  const { user } = useContext(UserContext);
+
+  if (user === "") {
+    return "";
+  }
+
   return (
     <div className="flex flex-row m-5">
       <Board />
