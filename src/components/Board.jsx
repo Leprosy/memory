4fb4acf632f741e3ea6981016279024b1ae173a1
Miguel Rojas <leprosy57@gmail.com>
2023-09-15 15:42:16 -0300

    Instructions

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -71,11 +71,11 @@ export const Board = () => {
     <div className="basis-3/4 relative">
       {images.length == 0
         ? (<h1 className="board__message">Loading board...</h1>)
-        : <>
-          <div className={`grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-items-center gap-3${hasWon ? " blur" : ""}`}>
-            {tiles}
-          </div>
-        </>
+        :
+        <div className={`grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 justify-items-center gap-3${hasWon ? " blur" : ""}`}>
+          {tiles}
+        </div>
+
       }
 
       {hasWon ? <h1 className="board__message">You win!</h1> : "" }
